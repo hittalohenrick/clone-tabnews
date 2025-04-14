224405a9dd80bdd81deb3cdeb451a33e23b36b88
Híttalo Henrick <henrickhittalo@gmail.com>
2025-04-14 12:10:16 +0000

    correção do texto do `index`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
  function Home() {
-    return <h1>Nos momentos de tensão busque sempre extrair algo positivo, não seja um simples passageiro da agônia.</h1>
+    return <h1>Nos momentos de tensão, busque sempre extrair algo positivo. Não seja um simples passageiro da agonia.</h1>
  }
 
  export default Home;
